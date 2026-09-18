e56427981a373dedff83ae5dbbdf3a8765d0fc12
zabarmawi <50752709+zabarmawi@users.noreply.github.com>
2026-09-18 22:35:23 +0300

    Add V9 service worker

---
+++ sw.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'facilitation-reflection-v9';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  const req = event.request;
+  const isNavigation = req.mode === 'navigate';
+
+  if (isNavigation) {
+    event.respondWith(
+      fetch(req, { cache: 'no-store' })
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy));
+          return response;
+        })
+        .catch(() =>
+          caches.match('./index.html').then(cached => cached || caches.match('./'))
+        )
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(req).then(cached => {
+      if (cached) return cached;
+      return fetch(req).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
+        return response;
+      });
+    })
+  );
+});
